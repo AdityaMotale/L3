b68112b7e44b5dafd8efd7d9c79bbf52e1a4d222
adii <77727680+AdityaMotale@users.noreply.github.com>
2025-06-14 19:45:39 +0530

    (popcnt) v1 function

--- popcnt/popcnt.c
+++ popcnt/popcnt.c
@@ -1,6 +1,18 @@
+#include <stdint.h>
 #include <stdio.h>
 
+uint32_t popcnt_v1(uint32_t x) {
+  uint32_t total = 0;
+
+  while (x) {
+    total++;
+    x &= (x - 1);
+  }
+
+  return total;
+}
+
 int main(void) {
-  printf("Hello\n");
+  printf("POPCNT 24 -> %d\n", popcnt_v1(13));
   return 0;
 }
