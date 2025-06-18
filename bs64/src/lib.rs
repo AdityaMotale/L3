88b5a80d6878fd19e0320c9fccceafeddef13d86
adii <77727680+AdityaMotale@users.noreply.github.com>
2025-06-18 12:28:31 +0530

    (bs64) complete impl of encoder

--- bs64/src/lib.rs
+++ bs64/src/lib.rs
@@ -1,10 +1,8 @@
 pub fn encode(input: &[u8]) -> String {
     const TABLE: &[u8; 64] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-
-    let chunks = input.chunks_exact(3);
     let mut output = String::new();
 
-    fn encode_chunk(chunk: &[u8], output: &mut String) {
+    for chunk in input.chunks_exact(3) {
         let c = (chunk[0] as u32) << 16 | (chunk[1] as u32) << 8 | chunk[2] as u32;
 
         let b1 = ((c >> 18) & 0x3F) as usize;
@@ -18,25 +16,33 @@ pub fn encode(input: &[u8]) -> String {
         output.push(TABLE[b4] as char);
     }
 
-    fn pad_to_3(chunk: &[u8]) -> [u8; 3] {
-        let mut c = [61u8; 3];
+    let rem = input.chunks_exact(3).remainder();
 
-        for (i, &b) in chunk.iter().enumerate() {
-            c[i] = b;
+    match rem.len() {
+        0 => { /* Do nothing */ }
+        1 => {
+            let c = (rem[0] as u32) << 16;
+            let b1 = ((c >> 18) & 0x3F) as usize;
+            let b2 = ((c >> 12) & 0x3F) as usize;
+
+            output.push(TABLE[b1] as char);
+            output.push(TABLE[b2] as char);
+            output.push_str("==");
         }
+        2 => {
+            let c = (rem[0] as u32) << 16 | (rem[1] as u32) << 8;
 
-        c
+            let b1 = ((c >> 18) & 0x3F) as usize;
+            let b2 = ((c >> 12) & 0x3F) as usize;
+            let b3 = ((c >> 6) & 0x3F) as usize;
+
+            output.push(TABLE[b1] as char);
+            output.push(TABLE[b2] as char);
+            output.push(TABLE[b3] as char);
+            output.push_str("=");
+        }
+        _ => unreachable!(),
     }
-
-    let remainder = chunks.remainder();
-
-    for chunk in chunks {
-        encode_chunk(chunk, &mut output);
-    }
-
-    let chunk = pad_to_3(remainder);
-
-    encode_chunk(&chunk, &mut output);
 
     output
 }
