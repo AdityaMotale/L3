f38316c12fbc8b10aa72c43eb0494c5d06710e5f
adii <77727680+AdityaMotale@users.noreply.github.com>
2025-06-29 21:43:41 +0530

    (y3) optimized tokenizer reading w/ BufReader

--- y3/src/lib.rs
+++ y3/src/lib.rs
@@ -1,30 +1,38 @@
 #![allow(dead_code)]
 
-use std::{fs::File, io::Read, path::PathBuf};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+const BUFFER_SIZE: usize = 1024 * 32; // 32 Kib
 
 pub struct Y3 {
-    file: PathBuf,
+    file: String,
 }
 
 impl Y3 {
-    pub fn new(path: PathBuf) -> Self {
-        Self { file: path }
+    pub fn new(path: &str) -> Self {
+        Self {
+            file: path.to_owned(),
+        }
     }
 
     pub fn tokenize(&self) -> std::io::Result<usize> {
-        let mut file = File::open(&self.file)?;
-        let mut buffer = [0u8; 1024];
-
+        let file = File::open(&self.file)?;
         let mut totale_bytes = 0usize;
+        let mut reader = BufReader::with_capacity(BUFFER_SIZE, file);
 
         loop {
-            let n = file.read(&mut buffer)?;
+            let buf = reader.fill_buf()?;
+            let n = buf.len();
 
             if n == 0 {
                 break;
             }
 
             totale_bytes += n;
+            reader.consume(n);
         }
 
         Ok(totale_bytes)
@@ -37,7 +45,7 @@ mod tests {
 
     #[test]
     fn test_valid_read() {
-        let y3 = Y3::new(PathBuf::from("asm.txt"));
+        let y3 = Y3::new("asm.txt");
 
         let n = y3.tokenize().unwrap();
 
