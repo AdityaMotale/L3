8a782e5a4d5f03a0983dc81b74102777d9e163e9
adii <77727680+AdityaMotale@users.noreply.github.com>
2025-06-29 13:47:14 +0530

    (xxhash) expanded traits

--- xxhash/src/lib.rs
+++ xxhash/src/lib.rs
@@ -13,6 +13,13 @@ impl IntoU32 for u8 {
     }
 }
 
+impl IntoU32 for usize {
+    #[inline(always)]
+    fn into_u32(self) -> u32 {
+        self as u32
+    }
+}
+
 pub(crate) trait IntoU64 {
     fn into_u64(self) -> u64;
 }
@@ -21,5 +28,12 @@ impl IntoU64 for u8 {
     #[inline(always)]
     fn into_u64(self) -> u64 {
         self.into()
+    }
+}
+
+impl IntoU64 for usize {
+    #[inline(always)]
+    fn into_u64(self) -> u64 {
+        self as u64
     }
 }
