031b0c7974ef0df2b0a77863e6d8e4a0fb3aa67c
adii <77727680+AdityaMotale@users.noreply.github.com>
2025-06-29 21:36:45 +0530

    (y3) tokenizer init

--- y3/src/lib.rs
+++ y3/src/lib.rs
@@ -1,5 +1,34 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#![allow(dead_code)]
+
+use std::{fs::File, io::Read, path::PathBuf};
+
+pub struct Y3 {
+    file: PathBuf,
+}
+
+impl Y3 {
+    pub fn new(path: PathBuf) -> Self {
+        Self { file: path }
+    }
+
+    pub fn tokenize(&self) -> std::io::Result<usize> {
+        let mut file = File::open(&self.file)?;
+        let mut buffer = [0u8; 1024];
+
+        let mut totale_bytes = 0usize;
+
+        loop {
+            let n = file.read(&mut buffer)?;
+
+            if n == 0 {
+                break;
+            }
+
+            totale_bytes += n;
+        }
+
+        Ok(totale_bytes)
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +36,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_valid_read() {
+        let y3 = Y3::new(PathBuf::from("asm.txt"));
+
+        let n = y3.tokenize().unwrap();
+
+        assert_eq!(n, 13695);
     }
 }
