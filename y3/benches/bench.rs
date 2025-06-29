ed2eeac4a92044d165be756c21a489a44c00ce4d
adii <77727680+AdityaMotale@users.noreply.github.com>
2025-06-29 22:03:56 +0530

    (y3) added benches

---
+++ y3/benches/bench.rs
@@ -0,0 +1,40 @@
+use criterion::{
+    criterion_group, criterion_main, measurement::WallTime, BenchmarkGroup, Criterion, Throughput,
+};
+use std::env::consts::ARCH;
+use y3::Y3;
+
+trait CriterionExt {
+    fn my_benchmark_group(&mut self, algo: &str, bench: &str) -> BenchmarkGroup<'_, WallTime>;
+}
+
+impl CriterionExt for Criterion {
+    fn my_benchmark_group(&mut self, algo: &str, bench: &str) -> BenchmarkGroup<'_, WallTime> {
+        self.benchmark_group(format!("arch-{ARCH}/algo-{algo}/bench-{bench}"))
+    }
+}
+
+mod v1 {
+    use super::*;
+
+    const NO_FILE: usize = 24;
+    const BYTES_SIZE: usize = 13695;
+
+    fn file_read(c: &mut Criterion) {
+        let mut g = c.my_benchmark_group("y3", "file_read");
+
+        for i in 0..NO_FILE {
+            let y3 = Y3::new("asm.txt");
+            g.throughput(Throughput::Bytes(BYTES_SIZE as _));
+
+            let id = format!("v1/iter-{i:02}");
+            g.bench_function(id, |b| b.iter(|| y3.tokenize()));
+        }
+
+        g.finish();
+    }
+
+    criterion_group!(bench, file_read);
+}
+
+criterion_main!(v1::bench);
