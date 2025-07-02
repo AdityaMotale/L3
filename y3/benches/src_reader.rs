475d10e7ea824a149ed8a69f623caee7333ca7a7
adii <77727680+AdityaMotale@users.noreply.github.com>
2025-07-02 19:25:44 +0530

    (y3) improved bench code

--- y3/benches/src_reader.rs
+++ y3/benches/src_reader.rs
@@ -1,7 +1,9 @@
+use criterion::BenchmarkId;
 use criterion::{
     criterion_group, criterion_main, measurement::WallTime, BenchmarkGroup, Criterion, Throughput,
 };
 use std::env::consts::ARCH;
+use std::{fs::File, hint::black_box, path::PathBuf};
 use y3::SrcReader;
 
 trait CriterionExt {
@@ -14,42 +16,35 @@ impl CriterionExt for Criterion {
     }
 }
 
-mod src_reader {
-    use super::*;
-    use criterion::BenchmarkId;
-    use std::{fs::File, hint::black_box, path::PathBuf};
+fn get_file_size(path: &PathBuf) -> usize {
+    let file = File::open(path).unwrap();
 
-    fn get_file_size(path: &PathBuf) -> usize {
-        let file = File::open(path).unwrap();
-
-        file.metadata().unwrap().len() as usize
-    }
-
-    fn file_read(c: &mut Criterion) {
-        let mut g = c.my_benchmark_group("y3", "src_reader");
-        let src_files = std::fs::read_dir("./ex_files").unwrap();
-
-        for f_name in src_files {
-            let path = f_name.unwrap().path();
-            let id = format!("{:?}", path.file_name().unwrap());
-            let size = get_file_size(&path);
-
-            g.throughput(Throughput::Bytes(size as u64));
-            g.bench_with_input(BenchmarkId::from_parameter(id), &path, |b, path| {
-                b.iter(|| {
-                    let mut sr = SrcReader::new(&path).unwrap();
-
-                    while let Some(chunk) = sr.get_chunk() {
-                        black_box(chunk);
-                    }
-                });
-            });
-        }
-
-        g.finish();
-    }
-
-    criterion_group!(bench, file_read);
+    file.metadata().unwrap().len() as usize
 }
 
-criterion_main!(src_reader::bench);
+fn file_read(c: &mut Criterion) {
+    let mut g = c.my_benchmark_group("y3", "src_reader");
+    let src_files = std::fs::read_dir("./ex_files").unwrap();
+
+    for f_name in src_files {
+        let path = f_name.unwrap().path();
+        let id = path.file_name().and_then(|name| name.to_str()).unwrap();
+        let size = get_file_size(&path);
+
+        g.throughput(Throughput::Bytes(size as u64));
+        g.bench_with_input(BenchmarkId::from_parameter(id), &path, |b, path| {
+            b.iter(|| {
+                let mut sr = SrcReader::new(&path).unwrap();
+
+                while let Some(chunk) = sr.get_chunk() {
+                    black_box(chunk);
+                }
+            });
+        });
+    }
+
+    g.finish();
+}
+
+criterion_group!(bench, file_read);
+criterion_main!(bench);
