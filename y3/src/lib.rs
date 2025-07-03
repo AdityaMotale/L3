acf345ff1974a862f315ab6e56989a4c591134b8
adii <77727680+AdityaMotale@users.noreply.github.com>
2025-07-03 22:06:01 +0530

    (y3) optimization for 1st pass w/ AVX2

--- y3/src/lib.rs
+++ y3/src/lib.rs
@@ -3,7 +3,11 @@
 use memmap::Mmap;
 use std::{fs::File, io, path::PathBuf};
 
+#[cfg(target_arch = "x86_64")]
+use core::arch::x86_64::*;
+
 const BUFFER_SIZE: usize = 1024 * 32; // 16 Kib
+const CHUNK_SIZE: usize = 16; // 16 bytes (used for SIMD)
 
 enum SrcType {
     InMem(Vec<u8>),
@@ -41,88 +45,66 @@ impl SrcReader {
         }
     }
 
-    pub fn get_chunk(&mut self) -> Option<&[u8]> {
-        match &self.src {
-            SrcType::InMem(buf) => {
-                if self.pos == self.len {
-                    return None;
-                }
-
-                let end = (self.pos + BUFFER_SIZE).min(self.len);
-                let slice = &buf[self.pos..end];
-
-                self.pos = end;
-
-                Some(slice)
-            }
-            SrcType::Mmap(mmap) => {
-                if self.pos == self.len {
-                    return None;
-                }
-
-                let end = (self.pos + BUFFER_SIZE).min(self.len);
-                let slice = &mmap[self.pos..end];
-
-                self.pos = end;
-
-                Some(slice)
-            }
+    pub fn get_chunk(&mut self) -> Option<[u8; CHUNK_SIZE]> {
+        if self.pos == self.len {
+            return None;
         }
+
+        let end = (self.pos + CHUNK_SIZE).min(self.len);
+        let slice = match &self.src {
+            SrcType::InMem(buf) => &buf[self.pos..end],
+            SrcType::Mmap(mmap) => &mmap[self.pos..end],
+        };
+
+        let mut chunk = [0u8; CHUNK_SIZE];
+
+        unsafe {
+            std::ptr::copy_nonoverlapping(slice.as_ptr(), chunk.as_mut_ptr(), slice.len());
+        }
+
+        self.pos = end;
+        Some(chunk)
     }
 }
 
-pub struct Tokenizer {
-    lookup: [u8; 256],
-}
+pub struct Tokenizer;
 
 impl Tokenizer {
-    const DELIM: u8 = 0b001000; // whitespaces, '_', '-', etc.
-
-    pub fn new() -> Self {
-        Self {
-            lookup: Self::build_lookup(),
-        }
-    }
-
-    pub fn tokenize(&self, path: &PathBuf) -> io::Result<Vec<u8>> {
+    pub fn tokenize(path: &PathBuf) -> io::Result<Vec<u8>> {
         const SPACE: u8 = b' ';
         let mut tokens: Vec<u8> = Vec::with_capacity(BUFFER_SIZE * 2);
         let mut src_reader = SrcReader::new(&path)?;
-        let mut tok_len: u8 = 0;
 
         while let Some(buf) = src_reader.get_chunk() {
-            for &ch in buf {
-                if self.lookup[ch as usize] & Self::DELIM != 0 {
-                    if tok_len == 1 {
-                        tokens.pop();
+            let mut output = [0u8; 16];
 
-                        continue;
-                    }
-
-                    tokens.push(SPACE);
-                    tok_len = 0;
-
-                    continue;
-                }
-
-                tokens.push(ch);
-                tok_len += 1;
+            unsafe {
+                Self::replace_delims_16_simple(buf.as_ptr(), output.as_mut_ptr());
             }
+
+            tokens.extend_from_slice(&output);
         }
 
         Ok(tokens)
     }
 
-    #[inline]
-    fn build_lookup() -> [u8; 256] {
-        let mut t = [0u8; 256];
+    #[target_feature(enable = "avx2")]
+    unsafe fn replace_delims_16_simple(input: *const u8, output: *mut u8) {
+        let orig = _mm_loadu_si128(input as *const __m128i);
 
-        // delimiters
-        for &b in &[b' ', b'\n', b'\r', b'\t', b'-', b'_'] {
-            t[b as usize] |= Self::DELIM;
-        }
+        let v_nl = _mm_cmpeq_epi8(orig, _mm_set1_epi8(b'\n' as i8));
+        let v_cr = _mm_cmpeq_epi8(orig, _mm_set1_epi8(b'\r' as i8));
+        let v_tab = _mm_cmpeq_epi8(orig, _mm_set1_epi8(b'\t' as i8));
+        let v_dash = _mm_cmpeq_epi8(orig, _mm_set1_epi8(b'-' as i8));
+        let v_us = _mm_cmpeq_epi8(orig, _mm_set1_epi8(b'_' as i8));
+        let mask1 = _mm_or_si128(v_nl, v_cr);
+        let mask2 = _mm_or_si128(v_tab, v_dash);
+        let mask = _mm_or_si128(_mm_or_si128(mask1, mask2), v_us);
 
-        t
+        let space = _mm_set1_epi8(b' ' as i8);
+        let result = _mm_blendv_epi8(orig, space, mask);
+
+        _mm_storeu_si128(output as *mut __m128i, result);
     }
 }
 
@@ -133,15 +115,14 @@ mod token_tests {
     use tempfile::NamedTempFile;
 
     #[test]
-    fn test_tiny_file() {
+    fn test_simple_text() {
         let mut temp_file = NamedTempFile::new().unwrap();
         temp_file.write(b"# Contact Hommes EMAIL").unwrap();
 
-        let expected_tokens = ["Contact", "Hommes", "EMAIL"];
+        let expected_tokens = ["#", "Contact", "Hommes", "EMAIL"];
         let path = temp_file.path().to_path_buf();
-        let tokenizer = Tokenizer::new();
 
-        let tokens = tokenizer.tokenize(&path).unwrap();
+        let tokens = Tokenizer::tokenize(&path).unwrap();
 
         let mut token: Vec<u8> = Vec::new();
         let mut idx: usize = 0;
@@ -161,79 +142,69 @@ mod token_tests {
         }
     }
 }
-
 #[cfg(test)]
 mod reader_tests {
     use super::*;
+    use std::fs::File;
     use std::io::Write;
     use tempfile::NamedTempFile;
 
     fn get_file_size(path: &PathBuf) -> usize {
         let file = File::open(path).unwrap();
-
         file.metadata().unwrap().len() as usize
     }
 
     #[test]
-    fn test_tiny_file() {
+    fn test_chunk_size_input() {
         let mut temp_file = NamedTempFile::new().unwrap();
-        temp_file
-            .write(b"# Contact: Onno Hommes EMAIL <ohommes@cmu.edu>.")
-            .unwrap();
+        let data = b"Contact OH Homme";
+
+        // make sure data is of chunk len
+        assert!(
+            data.len() == CHUNK_SIZE,
+            "input data must be of size CHUNK_SIZE, (INVALID INPUT)",
+        );
+
+        temp_file.write_all(data).unwrap();
 
         let path = temp_file.path().to_path_buf();
         let file_size = get_file_size(&path);
 
         let mut sr = SrcReader::new(&path).unwrap();
+        let buf = sr.get_chunk().expect("expected one chunk for tiny file");
 
-        // before reading the chunk
-        assert_eq!(sr.pos, 0, "start position should be zero");
-        assert_eq!(sr.len, file_size, "reader length should equal file size");
-
-        // read the only chunk
-        let buf = sr.get_chunk().expect("expected one chunk from tiny file");
-
-        assert_eq!(
-            buf.len(),
-            file_size,
-            "chunk length should equal entire file size"
-        );
-
-        // after reading the chunk
-        assert_eq!(sr.pos, file_size, "pos should now equal file size");
-        assert!(sr.get_chunk().is_none(), "reader should stay at EOF");
+        assert_eq!(buf.len(), CHUNK_SIZE);
+        assert_eq!(&buf[..file_size], data);
+        assert!(buf[file_size..].iter().all(|&b| b == 0));
+        assert!(sr.get_chunk().is_none());
     }
 
     #[test]
-    fn test_large_file() {
+    fn test_large_file_read() {
         let path = PathBuf::from("./ex_files/large.txt");
         let file_size = get_file_size(&path);
 
         let mut sr = SrcReader::new(&path).unwrap();
-
-        // before reading the chunk
-        assert_eq!(sr.pos, 0, "start position should be zero");
-        assert_eq!(sr.len, file_size, "reader length should equal file size");
-
-        let mut accumulated = 0;
+        let mut num_chunks = 0;
 
         while let Some(buf) = sr.get_chunk() {
-            assert!(!buf.is_empty(), "chunk should never be empty");
-            assert!(
-                buf.len() <= BUFFER_SIZE,
-                "chunk.len() = {}, but BUFFER_SIZE = {}",
-                buf.len(),
-                BUFFER_SIZE
-            );
-
-            accumulated += buf.len();
+            assert_eq!(buf.len(), CHUNK_SIZE);
+            num_chunks += 1;
         }
 
-        assert_eq!(sr.pos, file_size, "pos should now equal file size");
-        assert!(sr.get_chunk().is_none(), "reader should stay at EOF");
-        assert_eq!(
-            accumulated, file_size,
-            "sum of all chunk lengths should equal file size"
+        let covered = num_chunks * CHUNK_SIZE;
+
+        assert!(
+            covered >= file_size,
+            "covered={} should be ≥ file_size={}",
+            covered,
+            file_size
+        );
+        assert!(
+            covered < file_size + CHUNK_SIZE,
+            "covered={} should be < file_size+CHUNK_SIZE={}",
+            covered,
+            file_size + CHUNK_SIZE
         );
     }
 }
